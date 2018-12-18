8a6692e227fd84a185dee9a4638cd05739515384
schafkoepfer <38036591+schafkoepfer@users.noreply.github.com>
2018-12-18 16:11:20 +0100

    new gui eingebaut

--- gui/qt_gui.py
+++ gui/qt_gui.py
@@ -1,6 +1,8 @@
 """QT_GUI"""
 
 import multiprocessing
+import configparser
+
 
 from PyQt5 import QtWidgets, uic
 
@@ -24,17 +26,26 @@ class qt_ui(QtWidgets.QDialog):
         uic.loadUi('Startmenu.ui', self)
         os.chdir(old_working_dir)
 
+        #Click Handler
         self.startButton.clicked.connect(self.start_random)
         self.sosy.clicked.connect(self.start_sosy)
         self.stopButton.clicked.connect(self.stop_simulation)
         self.objcountslider.valueChanged.connect(self.update)
-        self.maxmassslider.valueChanged.connect(self.update)
+        self.minmass.valueChanged.connect(self.update)
+        self.maxmass.valueChanged.connect(self.update)
+        self.minrad.valueChanged.connect(self.update)
+        self.maxrad.valueChanged.connect(self.update)
         self.maxdistslider.valueChanged.connect(self.update)
         self.sunmulslider.valueChanged.connect(self.update)
-        # self.SpeedSlider.valueChanged.connect(self.update)
-        # self.SunMassSlider.valueChanged.connect(self.update)
+        self.connectbutton.clicked.connect(self.client_connect)
+        self.disconnectbutton.clicked.connect(self.client_disconnect)
 
-        # self.SpeedLabel.valuechanged.connect(self.update())
+        #Ip/port aus configfile lesen
+        conf = configparser.ConfigParser()
+        conf.read("connection_config.ini")
+        self.iptext.setText(conf['Connection']['ip'])
+        self.porttext.setText(conf['Connection']['port'])
+
         self.renderer_conn, self.simulation_conn = None, None
         self.render_process = None
         self.simulation_process = None
@@ -51,6 +62,11 @@ class qt_ui(QtWidgets.QDialog):
         self.maxdistdisp.display(self.maxdistslider.value())
         self.sunmuldisp.display(self.sunmulslider.value() / 10)
 
+        if self.maxmass.value() < self.minmass.value():
+            self.maxmass.setValue(self.minmass.value())
+        if self.maxrad.value() < self.minrad.value():
+            self.maxrad.setValue(self.minrad.value())
+
     def start_random(self):
         """
         Start the Randonm
@@ -65,6 +81,12 @@ class qt_ui(QtWidgets.QDialog):
         """
         self.start_simulation(True)
 
+    def client_connect(self):
+        a=True
+
+    def client_disconnect(self):
+        a=True
+
     def start_simulation(self, sosy):
         """
             Start simulation and render process connected with a pipe.
@@ -75,7 +97,10 @@ class qt_ui(QtWidgets.QDialog):
             context = s.initialize_random(self.objcountslider.value(),
                                           -self.maxdistslider.value() * 10**9,
                                           self.maxdistslider.value() + 10**9,
-                                          self.maxmassslider.value() * 10**24)
+                                          self.minrad.value() / 100,
+                                          self.maxrad.value() / 100,
+                                          self.minmass.value() * 10**24,
+                                          self.maxmass.value() * 10**24,)
 
         context.add_body_mass(0, self.sunmulslider.value() / 10)
         # context.add_speed(self.SpeedSlider.value()/10)
