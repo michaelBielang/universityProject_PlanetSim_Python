fe7972533c8b5a94eb35faebc815ef434d3b12f0
schmidtm <markus.schmidt@hs-augsbrug.de>
2018-12-18 14:45:09 +0100

    Verteilung, nicht funktional

---
+++ core/taskmanager.py
@@ -0,0 +1,42 @@
+from multiprocessing.managers import BaseManager
+from threading import Thread
+
+
+class TaskManager(BaseManager):
+    pass
+
+    def startup(self,context):
+
+        master_socket = int(12345)
+        self.task_queue = context.InputQueue
+        self.result_queue = context.OutputQueue
+        self.np_bodies = context.np_bodies
+
+        TaskManager.register('get_job_queue',
+                             callable = lambda:self.task_queue)
+        TaskManager.register('get_result_queue',
+                             callable = lambda:self.result_queue)
+        TaskManager.register('get_np_bodies',
+                             callable = lambda:self.np_bodies)
+        self.m = TaskManager(address = ('', master_socket),
+                        authkey = b'secret')
+
+        thread = Thread(target=self.runServer)
+        thread.start()
+
+    def runServer(self):
+        print('starting queue server, socket')
+        self.m.get_server().serve_forever()
+
+    def joinQueue(self):
+        self.task_queue.join()
+
+    def clientConnect(self):
+        server_ip = "localhost"
+        server_socket = int(12345)
+        TaskManager.register('get_job_queue')
+        TaskManager.register('get_result_queue')
+        TaskManager.register('get_np_bodies')
+        m = TaskManager(address=(server_ip, server_socket), authkey = b'secret')
+        m.connect()
+        return m
