d62224ba951c7da48f24454ccc60760de08e71a4
schmidtm <markus.schmidt@hs-augsbrug.de>
2018-12-22 15:17:44 +0100

    Test Cython

---
+++ setup.py
@@ -0,0 +1,11 @@
+from distutils.core import setup
+from Cython.Build import cythonize
+
+modules = ("core\\body.pyx", "core\calc.pyx","core\calc.pyx",
+           "core\context.pyx","core\simulation.pyx","core\\taskmanager.pyx",
+           "gui\opengl_simulation.pyx","gui\qt_gui.pyx")
+
+
+setup(
+    ext_modules = cythonize(modules)
+)
