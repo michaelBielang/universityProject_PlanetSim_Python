1442f2af7acd126632605bf1c857ed869afb0166
millerlu <lukas.miller@hs-augsburg.de>
2019-01-04 13:05:33 +0100

    merged master into current

--- setup.py
+++ setup.py
@@ -1,9 +1,9 @@
 from distutils.core import setup
 from Cython.Build import cythonize
 
-modules = ("core\\body.pyx", "core\calc.pyx","core\calc.pyx",
-           "core\context.pyx","core\simulation.pyx","core\\taskmanager.pyx",
-           "gui\opengl_simulation.pyx","gui\qt_gui.pyx")
+modules = ("core/body.pyx", "core/calc.pyx","core/calc.pyx",
+           "core/context.pyx","core/simulation.pyx","core/taskmanager.pyx",
+           "gui/opengl_simulation.pyx","gui/qt_gui.pyx")
 
 
 setup(
